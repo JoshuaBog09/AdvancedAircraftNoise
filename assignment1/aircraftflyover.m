724f3cb42a202375ebdbd962d48e61104988e63b
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2023-12-01 15:08:23 +0100

    Pressure flyover

--- assignment1/aircraftflyover.m
+++ assignment1/aircraftflyover.m
@@ -2,4 +2,18 @@ clear;
 
 load('aircraft_flyover_microphone_assignment1.mat');
 
+y = aircraft_flyover_microphone_assignment1(1,:);
 
+t_begin = 0;
+samplerate = 40000;
+delta_t = 1 / samplerate;
+
+t_end = delta_t*length(y) - delta_t;
+
+t = t_begin:delta_t:t_end;
+
+%% Part I: Figure 1 --> pressure over time
+figure();
+plot(t,y)
+
+%% Part II
