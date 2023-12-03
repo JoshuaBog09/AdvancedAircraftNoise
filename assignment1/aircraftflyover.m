3efdbbf59f6e4db4212e8f1bec6523790a1f406a
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2023-12-03 17:50:35 +0100

    integ

--- assignment1/aircraftflyover.m
+++ assignment1/aircraftflyover.m
@@ -91,11 +91,16 @@ f = 0: freq_resolution: (N-1)*freq_resolution; %create the frequency x-axis
 psd = (time_resolution^2/time_reso(end))*(abs(Y).^2);
 
 
-figure()
+figure(1);
 plot(f, psd, '+k'); h = get(gcf, 'Children'); set(h, fontsize, '14'); xlabel('frequency');
-ylabel('PSD'); title('PSD 0-40k'); gird; axis([0 samplefrequency 0 0.1])
+ylabel('PSD'); title('PSD 0-40k'); grid; axis([0 samplefrequency 0 0.1]);
 
 % Half sided 
 
 f_half = 0:freq_resolution:(N-1)*freq_resolution/2;
 psd_half = ((1/sqrt(2))^2) * (2^2) * psd(1:length(f_half));
+
+figure(2);
+plot(f_half, psd_half, '+k'); h = get(gcf, 'Children'); set(h, fontsize, '14'); xlabel('frequency');
+ylabel('PSD'); title('PSD 0-40k'); grid; axis([0 samplefrequency 0 0.1])
+
