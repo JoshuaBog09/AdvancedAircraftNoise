7940a6e62536dbff46366c13838965d33e1769b1
elisabethoost <elisabethoosthoek@gmail.com>
2023-12-26 17:44:13 +0100

    Update main.m
    flaps equations added

--- assignment3/main.m
+++ assignment3/main.m
@@ -51,8 +51,36 @@ F = 0.0577 .* (S.^2) .* ((0.25*S.^2) + 1).^(-1.5);
 % Directivity funtion
 D = (3/2)*(sin(theta))^2;
 
-% 
+%% Flaps formulas
+K = 2.787*10-4;
+a = 6;
+delta_f = pi/6; %ASSUMPTION
+G = A_f/b_f^2*(sin(delta_f))^2;
+L = A_f/b_f;
+
+% Power function
+P = K*(M^a)*G*rho*(c^3)*(b_w^2);
+
+% Strouhal number
+S = (f * L * (1 - M*cos(theta))) / (M*c);
+
+% Spectral function
+F = zeros(size(S));
+
+% Apply the conditions for each range
+F(S < 2) = 0.0480 * S(S < 2);
+F((2 <= S) & (S <= 20)) = 0.1406 * S((2 <= S) & (S <= 20)).^(-0.55);
+F(S > 20) = 216.49 * S(S > 20).^(-3);
+
+% Directivity funtion
+D = 3*(sin(delta_f)*cos(theta)+cos(delta_f)*sin(theta)*cos(phi))^2;
+
 psquared = (rho*c*P*D*F) / (4*(pi^2)*(r^2)*(1-M*cos(theta))^4);
+
+%%
+
+% 
+%psquared = (rho*c*P*D*F) / (4*(pi^2)*(r^2)*(1-M*cos(theta))^4);
 
 figure();
 semilogx(f,psquared)
