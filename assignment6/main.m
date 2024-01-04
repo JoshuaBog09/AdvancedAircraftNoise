23c7871460a7a5926841134cec596543c87e76c7
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2024-01-04 15:10:41 +0100

    Start q1 assignment 6

---
+++ assignment6/main.m
@@ -0,0 +1,41 @@
+%% Assignment 6: Advanced Aircraft Noise
+% By: Elisabeth and Joshua
+
+clear;
+
+%% Excersise 1
+
+f = 0:10:100000;
+
+R = 24;
+L = 20e-3;
+
+s = tf('s');
+
+H1 = (s*L) / (R + s*L);
+H2 = R / (R + s*L);
+
+% Bodeplot for first transfer function
+% Identified to be High pass filter (HPF)
+figure(1);
+bp1 = bodeplot(H1);
+setoptions(bp1,'FreqUnits','Hz');
+
+% Bodeplot for second transfer function
+% Identified to be Low pass filter (LPF)
+figure(2);
+bp2 = bodeplot(H2);
+setoptions(bp2,'FreqUnits','Hz');
+
+% Long methode to check bode plots
+%c1 = 20*log10(R ./ sqrt(R^2 + (2*pi*f).^2 * L^2));
+c2 = R ./ (R + 2*pi*f*L*1i);
+c1 = (2*pi*f*L*1i) ./ (R + (2*pi*f*L*1i));
+
+figure(3);
+semilogx(f, 20*log10(abs(c1)))
+hold on
+semilogx(f, 20*log10(abs(c2)))
+
+%% Excersise 2
+
