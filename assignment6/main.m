bc2e66d92619f654379eee39fcaa58b772702164
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2024-01-04 15:27:54 +0100

    Check for phase TODO add fc

--- assignment6/main.m
+++ assignment6/main.m
@@ -32,10 +32,20 @@ setoptions(bp2,'FreqUnits','Hz');
 c2 = R ./ (R + 2*pi*f*L*1i);
 c1 = (2*pi*f*L*1i) ./ (R + (2*pi*f*L*1i));
 
+% Checker for phase
+
+cp2 = 0 - atan((2*pi*f*L)./(R));
+cp1 = pi/2 - atan((2*pi*f*L)./(R));
+
 figure(3);
 semilogx(f, 20*log10(abs(c1)))
 hold on
 semilogx(f, 20*log10(abs(c2)))
+
+figure(4);
+semilogx(f, cp2*180/pi)
+hold on
+semilogx(f, cp1*180/pi)
 
 %% Excersise 2
 
