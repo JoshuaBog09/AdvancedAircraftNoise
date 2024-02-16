ea229726450a4bfc2e246478a7fb70a06ad78096
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2024-02-16 14:06:50 +0100

    Init para

---
+++ assignment5/main.m
@@ -0,0 +1,11 @@
+% Assignment 5: Advanced Aircraft Noise
+
+clear;
+
+load('Array/aircraft_noise_data_overhead_2020.mat');
+
+h = 64.87;      % [m]
+fs = 40000;     % [Hz]
+n_mic = 32;     % [-]
+
+%% Main code
